74508f1518b9cdd4def9ec531a6c46eca0134cf1
Luciano Piccoli <lpiccoli@slac.stanford.edu>
2016-10-05 12:10:58 -0700

    This version is using easylogging++; it read Faults and DigitalFaultStates as is from database - which is wrong. Database must have generic faults and states

---
+++ src/log.h
@@ -0,0 +1,13 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include "easylogging++.h"
+#undef _ELPP_STD_THREAD_AVAILABLE
+#define _ELPP_STD_THREAD_AVAILABLE 0
+
+_INITIALIZE_EASYLOGGINGPP
+using namespace easyloggingpp;
+
+#endif
+
+
